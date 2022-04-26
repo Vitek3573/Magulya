66a487a611504e8d0c240bc597ca267c40617565
Vitek3573 <vitya_fetisov@mail.ru>
2022-04-26 21:51:30 +0300

    create project

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,37 @@
 $(function () {
-    $('.reviews__slider').slick({
+  $('.reviews__slider').slick({
     arrows: false,
     dots: true,
-    slidesToShow: 3,
+    slidesToShow: 4,
     slidesToScroll: 1,
-    });
+    responsive: [
+      {
+        breakpoint: 841,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 715,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 586,
+        settings: {
+          slidesToShow: 1,
+        }
+      },
+    ]
+  });
+  $('.menu__btn').on('click', function () {
+    $('.menu__list').toggleClass('menu__list--active');
+  });
+  $(".menu a").on("click", function (e) {
+    e.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 1500);
+  });
 });
